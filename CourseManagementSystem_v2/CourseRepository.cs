6e4813bc9efb36a2678da448bd1eebbc153a1fd7
UT01314 <UT01314@UNICOMTIC60>
2024-10-11 16:53:25 +0530

    try catch addwed

--- CourseManagementSystem_v2/CourseRepository.cs
+++ CourseManagementSystem_v2/CourseRepository.cs
@@ -13,103 +13,144 @@ namespace CourseManagementSystem_v2
 
         public void CreateCourse(string courseId, string title, string Duration, decimal Price)
         {
-            using (var connection = new SqlConnection(connectionstring))
+            try
             {
-                connection.Open();
-                var command = connection.CreateCommand();
-                command.CommandText = @"insert into  Courses(CourseId,Title,Duration,Price) values(@courseId,@title,@Duration,@Price );";
-                command.Parameters.AddWithValue("@courseId", courseId);
-                command.Parameters.AddWithValue("@title", title);
-                command.Parameters.AddWithValue("@Duration", Duration);
-                command.Parameters.AddWithValue("@Price", Price);
-                command.ExecuteNonQuery();
+                using (var connection = new SqlConnection(connectionstring))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"insert into  Courses(CourseId,Title,Duration,Price) values(@courseId,@title,@Duration,@Price );";
+                    command.Parameters.AddWithValue("@courseId", courseId);
+                    command.Parameters.AddWithValue("@title", title);
+                    command.Parameters.AddWithValue("@Duration", Duration);
+                    command.Parameters.AddWithValue("@Price", Price);
+                    command.ExecuteNonQuery();
 
+                }
+            }
+            catch (Exception ex) 
+            {
+               Console.WriteLine(ex.Message);
             }
 
         }
         public void ReadCourses()
         {
-            using(var connection = new SqlConnection(connectionstring))
+
+
+            try
             {
-                connection.Open();
-                var command = connection.CreateCommand();
-                command.CommandText = @"select* from Courses";
-                using (var reader = command.ExecuteReader()) 
+                using (var connection = new SqlConnection(connectionstring))
                 {
-                    while (reader.Read()) 
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"select* from Courses";
+                    using (var reader = command.ExecuteReader())
                     {
-                        Console.WriteLine($"courseId{reader.GetString(0)} title{reader.GetString(1)}Duration{reader.GetString(2)} price {reader.GetDecimal(3)}");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"courseId{reader.GetString(0)} title{reader.GetString(1)}Duration{reader.GetString(2)} price {reader.GetDecimal(3)}");
+                        }
+
                     }
-                
                 }
             }
+            catch (Exception ex) {
+             Console.WriteLine(ex.Message);
+            }
+           
         }
         public void getcoursebyid(int id)
         {
-          using( var connection = new SqlConnection(connectionstring))
+            try
             {
-                connection.Open();
-                var command = connection.CreateCommand();
-                command.CommandText = @"select* from Courses where courseId=@courseId";
-                command.Parameters.AddWithValue("@courseId", id);
-                using (var reader = command.ExecuteReader())
+                using (var connection = new SqlConnection(connectionstring))
                 {
-                    if(reader.Read())
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"select* from Courses where courseId=@courseId";
+                    command.Parameters.AddWithValue("@courseId", id);
+                    using (var reader = command.ExecuteReader())
                     {
-                        Console.WriteLine($"courseId{reader.GetString(0)} title{reader.GetString(1)}Duration{reader.GetString(2)} price {reader.GetDecimal(3)}");
+                        if (reader.Read())
+                        {
+                            Console.WriteLine($"courseId{reader.GetString(0)} title{reader.GetString(1)}Duration{reader.GetString(2)} price {reader.GetDecimal(3)}");
+                        }
+
                     }
 
+
                 }
-
-
             }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+           
         }
 
         public void UpdateCourse(string courseId, string title, string Duration, decimal Price)
         {
-            using (var connection = new SqlConnection(connectionstring))
+
+
+            try
             {
-                connection.Open();
-                var command = connection.CreateCommand();
-                command.CommandText = @"UPDATE Courses SET title=@title, Duration=@Duration,Price=@Price where courseId=courseId ";
-                command.Parameters.AddWithValue("@courseId", courseId);
-                command.Parameters.AddWithValue("@title", title);
-                command.Parameters.AddWithValue("@Duration", Duration);
-                command.Parameters.AddWithValue("@Price", Price);
-                   var rowafect=command.ExecuteNonQuery();
-                if (rowafect > 0)
+                using (var connection = new SqlConnection(connectionstring))
                 {
-                    Console.WriteLine("Course Updated succsesfuly");
-                }
-                else {
-                    Console.WriteLine("coures not found");
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"UPDATE Courses SET title=@title, Duration=@Duration,Price=@Price where courseId=courseId ";
+                    command.Parameters.AddWithValue("@courseId", courseId);
+                    command.Parameters.AddWithValue("@title", title);
+                    command.Parameters.AddWithValue("@Duration", Duration);
+                    command.Parameters.AddWithValue("@Price", Price);
+                    var rowafect = command.ExecuteNonQuery();
+                    if (rowafect > 0)
+                    {
+                        Console.WriteLine("Course Updated succsesfuly");
+                    }
+                    else
+                    {
+                        Console.WriteLine("coures not found");
+
+                    }
 
                 }
-
             }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+          
         }
         public void DeleteCourse(string courseId) 
         {
-            using (var connection = new SqlConnection(connectionstring))
+
+
+
+            try
             {
-                connection.Open();
-                var command = connection.CreateCommand();
-                command.CommandText = @"DELETE* from Courses where courseId=@courseId";
-                command.Parameters.AddWithValue("@courseId", courseId);
-                var rowafect = command.ExecuteNonQuery();
-                if (rowafect > 0)
+                using (var connection = new SqlConnection(connectionstring))
                 {
-                    Console.WriteLine("Course Delete succsesfuly");
-                }
-                else
-                {
-                    Console.WriteLine("coures not found");
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = @"DELETE* from Courses where courseId=@courseId";
+                    command.Parameters.AddWithValue("@courseId", courseId);
+                    var rowafect = command.ExecuteNonQuery();
+                    if (rowafect > 0)
+                    {
+                        Console.WriteLine("Course Delete succsesfuly");
+                    }
+                    else
+                    {
+                        Console.WriteLine("coures not found");
+
+                    }
+
+
 
                 }
-
-
-
-            }
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+                    }
 
         }
     }
